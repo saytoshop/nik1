5e98752c1ad5ca611ca94405e6d688a5c8c3dc05
Oleg Spiridonov <ospiridonov@stack-kazan.net>
2019-06-25 17:19:09 +0300

    alpha

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-$(".collapsed").click(function(){
+$(".collapsed").click(function() {
   $(this).toggleClass("collapsed");
+});
+
+$(document).ready(function() {
+  $(".carousel1").owlCarousel({
+    loop: true,
+    items: 1,
+    nav: true,
+    lazyLoad: true,
+    navText: []
+  });
+  $(".news__slider").owlCarousel({
+    loop: true,
+    items: 2,
+    margin: 20,
+    nav: true,
+    navText: [],
+    responsive:{
+        0:{
+            items:1
+        },
+        1024:{
+            items:2
+        }
+    }
+  });
 });
