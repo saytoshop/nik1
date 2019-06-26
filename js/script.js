1295b9c08c4a0f87d758df4a7d894d807d7abb51
Oleg Spiridonov <ospiridonov@stack-kazan.net>
2019-06-26 12:12:07 +0300

    beta

--- js/script.js
+++ js/script.js
@@ -16,13 +16,37 @@ $(document).ready(function() {
     margin: 20,
     nav: true,
     navText: [],
-    responsive:{
-        0:{
-            items:1
-        },
-        1024:{
-            items:2
-        }
+    responsive: {
+      0: {
+        items: 1
+      },
+      1024: {
+        items: 2
+      }
     }
   });
+});
+
+
+$(".scroll").click(function(e) {
+  e.preventDefault();
+  var hash = $(this).attr("href");
+  var target = $(hash);
+$(".mobileMenu").removeClass("active");
+  $("html, body").animate({
+    scrollTop: target.offset().top - 10
+  }, 500, function() {
+    //       window.location.hash = hash;
+
+  });
+});
+
+$(".mobileMenuToggle").click(function (e) {
+  e.preventDefault();
+  $(".mobileMenu").toggleClass("active");
+});
+
+$(".mobileMenu__close").click(function (e) {
+  e.preventDefault();
+  $(".mobileMenu").toggleClass("active");
 });
